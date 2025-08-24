4b7c5c3964b8b7898610d5e5cfb169cb67c37373
sarvam <sarvam207@gmail.com>
2025-08-25 00:55:30 +0530

    'try_Updates'

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'taskmate.urls'
 
