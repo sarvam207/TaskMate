b880cb8a4e7b96940d5e31d30d53bd6a438a3bd0
sarvam <sarvam207@gmail.com>
2025-08-26 18:37:09 +0530

    Changed about us

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST", default='localhost'),
-        "PORT": env("DB_PORT", default='5432'),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 # Password validation
